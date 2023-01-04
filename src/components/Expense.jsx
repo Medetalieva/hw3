2eb7d933391f5333196f8f7b14e1e9c1d2cdaef8
Medetalieva Dilnaz <apple@MacBook-Air-Medetalieva.local>
2023-01-04 17:21:07 +0600

    create expense items and expense date components

---
+++ src/components/Expense.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { ExpenseItem } from "./ExpenseItem"
+ 
+
+const Expense = (props) => {   
+   return(
+        <div >
+            <ExpenseItem/>
+            <ExpenseItem/>
+            <ExpenseItem/>
+            
+        </div>
+    )
+}
+
+export default Expense
+
+
+ 
